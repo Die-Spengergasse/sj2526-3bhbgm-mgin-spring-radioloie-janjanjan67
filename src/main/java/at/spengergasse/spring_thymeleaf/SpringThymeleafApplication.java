5316acad54f23c09f191eb22617ec8ca0383527a
Jan <buk230363@spengergasse.at>
2026-03-26 15:59:43 +0100

    v.01

--- src/main/java/at/spengergasse/spring_thymeleaf/SpringThymeleafApplication.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/SpringThymeleafApplication.java
@@ -1,7 +1,13 @@
 package at.spengergasse.spring_thymeleaf;
 
+import at.spengergasse.spring_thymeleaf.entities.Device;
+import at.spengergasse.spring_thymeleaf.entities.DeviceRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringThymeleafApplication {
@@ -10,4 +16,27 @@ public class SpringThymeleafApplication {
         SpringApplication.run(SpringThymeleafApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner initData(DeviceRepository deviceRepository) {
+        return args -> {
+            if (deviceRepository.count() == 0) {
+                Device mri = new Device();
+                mri.setId("MR1");
+                mri.setType("MR");
+                mri.setLocation("101");
+
+                Device ct = new Device();
+                ct.setId("CT1");
+                ct.setType("CT");
+                ct.setLocation("102");
+
+                Device xray = new Device();
+                xray.setId("RX1");
+                xray.setType("Röntgen");
+                xray.setLocation("103");
+
+                deviceRepository.saveAll(List.of(mri, ct, xray));
+            }
+        };
+    }
 }
--- src/main/java/at/spengergasse/spring_thymeleaf/entities/Patient.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/entities/Patient.java
@@ -13,15 +13,50 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String name;
+    private String svnr;
+    private String firstName;
+    private String lastName;
+    private String gender;
     private LocalDate birth;
 
-    public String getName() {
-        return name;
+    public int getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSvnr() {
+        return svnr;
+    }
+
+    public void setSvnr(String svnr) {
+        this.svnr = svnr;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     public LocalDate getBirth() {
